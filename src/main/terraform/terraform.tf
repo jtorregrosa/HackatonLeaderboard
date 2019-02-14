9f0d93c6205749519cab05b908447660295ebb02
Samuel Ortiz Reina <sortiz@nouss.eu>
2019-02-14 23:03:13 +0100

    Update deploy on port 80

--- src/main/terraform/terraform.tf
+++ src/main/terraform/terraform.tf
@@ -1,14 +1,29 @@
-resource "azurerm_network_security_rule" "default" {
+resource "azurerm_network_security_rule" "8080" {
   provider = "azurerm.webapp"
   resource_group_name         = "${data.azurerm_network_security_group.default.resource_group_name}"
   network_security_group_name = "${data.azurerm_network_security_group.default.name}"
-  name                        = "Nouss-Lab-HackatonLeaderboard-WebApp"
-  priority                    = 1080
+  name                        = "Nouss-Lab-HackatonLeaderboard-WebApp-8080"
+  priority                    = 1088
   direction                   = "Inbound"
   access                      = "Allow"
   protocol                    = "Tcp"
   source_port_range           = "*"
   destination_port_range      = "8080"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+}
+
+resource "azurerm_network_security_rule" "80" {
+  provider = "azurerm.webapp"
+  resource_group_name         = "${data.azurerm_network_security_group.default.resource_group_name}"
+  network_security_group_name = "${data.azurerm_network_security_group.default.name}"
+  name                        = "Nouss-Lab-HackatonLeaderboard-WebApp-80"
+  priority                    = 1080
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
 }
